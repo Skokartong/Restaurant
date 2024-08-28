cc6e370a5051df015cfc8c90ee23d4f9f69f4799
Skokartong <amanda.olving@gmail.com>
2024-08-28 17:15:15 +0200

    Start implementing interfaces in Services

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,6 +1,41 @@
-﻿namespace Restaurant.Services
+﻿using Restaurant.Data.Repos.IRepos;
+using Restaurant.Models;
+using Restaurant.Models.DTOs;
+using Restaurant.Services.IServices;
+
+namespace Restaurant.Services
 {
-    public class CustomerService
+    public class CustomerService : ICustomerService
     {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public async Task AddCustomerAsync(CustomerDTO customerDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteCustomerAsync(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CustomerDTO> SearchCustomerAsync(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CustomerDTO>> SeeAllCustomersAsync(string restaurantName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CustomerDTO> UpdateCustomerAsync(int customerId, Customer updatedCustomer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/IServices/ICustomerService.cs
+++ Services/IServices/ICustomerService.cs
@@ -1,6 +1,14 @@
-﻿namespace Restaurant.Services.IServices
+﻿using Restaurant.Models;
+using Restaurant.Models.DTOs;
+
+namespace Restaurant.Services.IServices
 {
     public interface ICustomerService
     {
+        Task AddCustomerAsync(CustomerDTO customerDTO);
+        Task DeleteCustomerAsync(int customerId);
+        Task<CustomerDTO> UpdateCustomerAsync(int customerId, Customer updatedCustomer);
+        Task<IEnumerable<CustomerDTO>> SeeAllCustomersAsync(string restaurantName);
+        Task<CustomerDTO> SearchCustomerAsync(int customerId);
     }
 }
